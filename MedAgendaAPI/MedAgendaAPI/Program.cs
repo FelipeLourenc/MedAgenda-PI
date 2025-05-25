aad8ab158bdfc92edc5c5af5020e2414e87f06a0
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2025-05-25 16:19:29 -0300

    doc: ativa Swagger.

--- MedAgendaAPI/MedAgendaAPI/Program.cs
+++ MedAgendaAPI/MedAgendaAPI/Program.cs
@@ -6,28 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ClinicaContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Adiciona controllers
 builder.Services.AddControllers();
 
-// Configurar Swagger para documentação (opcional)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Aplica as migrações pendentes automaticamente
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ClinicaContext>();
     db.Database.Migrate();
 }
 
-// Middleware para Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Ativa o Swagger em todos os ambientes.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
